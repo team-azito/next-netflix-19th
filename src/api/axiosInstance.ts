d999f1af9ca8dc2c7718d41c977d89da88eaa29d
youdame <112458620+youdame@users.noreply.github.com>
2024-05-07 10:50:37 +0900

    Feat : 폴더 구조 잡기 및 axios 초기 설정

---
+++ src/api/axiosInstance.ts
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+export const instance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: { 'Content-Type': 'application/json', authorization: `Bearer ${process.env.ACCESS_TOKEN}` },
+});
---
+++ src/types/svgr.d.ts
@@ -0,0 +1,6 @@
+declare module '*.svg' {
+  import type { ReactElement, SVGProps } from 'react';
+
+  const content: (props: SVGProps<SVGElement> & { alt: string }) => ReactElement;
+  export default content;
+}
