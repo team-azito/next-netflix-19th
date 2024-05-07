101e16a8ba80f5eb0c4438916f24090f4f016f1f
youdame <112458620+youdame@users.noreply.github.com>
2024-05-07 14:11:51 +0900

    Style : 글로벌 css 및 색상 등록 #1

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,16 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        white: '#ffffff',
+        black: '#000000',
+        gray: {
+          10: '#8C8787',
+          20: '#424242',
+        },
       },
     },
   },
