f90880cad345a543a00beafa57de1ff65a6826db
youdame <112458620+youdame@users.noreply.github.com>
2024-05-08 23:10:36 +0900

    Style : 조건부 스타일링 추가

--- src/components/home/MovieSection.tsx
+++ src/components/home/MovieSection.tsx
@@ -7,18 +7,24 @@ const MovieSection = async ({ category }: { category: keyof typeof movieSectionT
   const moviesData = await getMovies(category);
   const posterBaseUrl = process.env.NEXT_PUBLIC_POSTER_BASE_URL;
 
+  // 조건부 스타일 적용을 위한 변수
+  const isPreview = category === "upcoming";
+  const titleStyle = isPreview ? "text-26pxr font-bold" : "text-21pxr font-bold"; // 글씨 크기 조정
+  const imageWrapperStyle = isPreview ? "min-w-102pxr h-102pxr" : "min-w-103pxr h-161pxr"; // 이미지 스타일 조정
+  const imageStyle = isPreview ? "rounded-full" : "rounded-sm"; // 이미지 스타일 조정
+
   return (
     <section className="flex-column w-full gap-14pxr">
-      <h2 className="text-21pxr font-bold">{movieSectionTitle[category]}</h2>
+      <h2 className={titleStyle}>{movieSectionTitle[category]}</h2>
       <div className="flex gap-7pxr overflow-auto">
         {moviesData.results.map((movie) => {
           if (!movie.poster_path) return null;
           return (
-            <Link href={`/media/${movie.id}`} key={movie.id} className="min-w-103pxr h-161pxr relative">
+            <Link href={`/media/${movie.id}`} key={movie.id} className={`${imageWrapperStyle} relative`}>
               <Image
                 alt={`영화 포스터 이미지: ${movie.title}`}
                 fill
-                className="rounded-sm"
+                className={imageStyle}
                 src={`${posterBaseUrl}${movie.poster_path}`}
               />
             </Link>
