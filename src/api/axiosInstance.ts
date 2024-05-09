ebd3833d3a0f80e64b5472d87eabb32d5af37b99
youdame <112458620+youdame@users.noreply.github.com>
2024-05-09 15:22:53 +0900

    빌드 에러 해결 중

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -2,10 +2,7 @@ import axios from "axios";
 
 export const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-  headers: {
-    "Content-Type": "application/json",
-    authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYjg5NWI5YjViODM5MzdlZTcyYmY2ZjVmMmZmMjJiNyIsInN1YiI6IjY2Mzk3ZTQ4NWFkNzZiMDEyOTA2MGE1YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2FOobN6UIP5-FaGBTj1wYb4S1GD2279JuwpGBxUYPNo`,
-  },
+  headers: { "Content-Type": "application/json", authorization: `Bearer ${process.env.NEXT_ACCESS_TOKEN}` },
 });
 
 // 항상 패칭 요청을 보내면 response.data가 값이 리턴되게
