959cbcb1a895105410120e3e54335fa569aa8e63
CSE-pebble <CSE.pebble@gmail.com>
2024-05-10 22:59:11 +0900

    Style: 아이콘 추가 및 CSS 적용

--- src/components/home/MoviePlay.tsx
+++ src/components/home/MoviePlay.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
+import PlusIcon from "#/icons/home/plus.svg";
+import PlayIcon from "#/icons/home/play.svg";
+import InfoIcon from "#/icons/home/info.svg";
 
 const MoviePlay = ({ movieData }) => {
   const posterBaseUrl = process.env.NEXT_PUBLIC_POSTER_BASE_URL;
@@ -10,23 +13,31 @@ const MoviePlay = ({ movieData }) => {
   return (
     <section className="flex-column items-center mb-43pxr gap-11pxr">
       <div className="h-375pxr w-full relative">
-        <Image src={`${posterBaseUrl}${data.poster_path}`} alt={`영화 포스터 이미지: ${data.title}`} fill />
+        <Image
+          src={`${posterBaseUrl}${data.poster_path}`}
+          alt={`영화 포스터 이미지: ${data.title}`}
+          fill
+          className="bg-gradient-to-b"
+        />
       </div>
       <div>
-        {/* icon */}
         <span>#{randomIndex} in Korea Today</span>
       </div>
       <div className="flex gap-50pxr">
-        <div>
-          {/* icon */}
+        <div className="flex-center flex-column">
+          <PlusIcon />
           My List
         </div>
         <Link href={`/media/${data.id}`}>
-          {/* icon */}
-          Play
+          <div className="flex-center h-45pxr w-110pxr bg-gray-30 rounded-md">
+            <div className="text-black flex-center gap-15pxr">
+              <PlayIcon />
+              Play
+            </div>
+          </div>
         </Link>
-        <div>
-          {/* icon */}
+        <div className="flex-center flex-column">
+          <InfoIcon />
           Info
         </div>
       </div>
