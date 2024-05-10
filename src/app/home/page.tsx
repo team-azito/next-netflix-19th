629864659e2f6f3c15d0a92afbdddf46c6c0b649
CSE-pebble <CSE.pebble@gmail.com>
2024-05-10 20:26:30 +0900

    Feat: MoviePlay 기능 구현 (스타일 미적용)

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,6 +2,7 @@ import MovieSection from "@/components/home/MovieSection";
 import HomeHeader from "@/components/home/HomeHeader";
 import { getMovies } from "@/api/home";
 import { MovieCategory } from "@/types/common";
+import MoviePlay from "@/components/home/MoviePlay";
 
 const HomePage = async () => {
   const categories: MovieCategory[] = ["upcoming", "now_playing", "popular", "top_rated"];
@@ -14,9 +15,12 @@ const HomePage = async () => {
     data: moviesResults[index],
   }));
 
+  const upcomingMoviesData = moviesData.find((item) => item.category === "upcoming");
+
   return (
     <div className="flex-column w-full pl-16pxr">
       <HomeHeader />
+      {upcomingMoviesData && <MoviePlay movieData={upcomingMoviesData.data} />}
       <div className="flex-column gap-22pxr">
         {moviesData.map(({ category, data }) => (
           <MovieSection key={category} category={category} movieData={data} />
---
+++ src/components/home/MoviePlay.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const MoviePlay = ({ movieData }) => {
+  const posterBaseUrl = process.env.NEXT_PUBLIC_POSTER_BASE_URL;
+
+  const randomIndex = Math.floor(Math.random() * movieData.results.length) + 1; // 랜덤 인덱스 생성
+  const data = movieData.results[randomIndex];
+
+  return (
+    <section className="flex-column items-center mb-43pxr gap-11pxr">
+      <Image
+        src={`${posterBaseUrl}${data.poster_path}`}
+        alt={`영화 포스터 이미지: ${data.title}`}
+        width={375}
+        height={397}
+      />
+      <div>
+        {/* icon */}
+        <span>#{randomIndex} in Korea Today</span>
+      </div>
+      <div className="flex gap-50pxr">
+        <div>
+          {/* icon */}
+          My List
+        </div>
+        <Link href={`/media/${data.id}`}>
+          {/* icon */}
+          Play
+        </Link>
+        <div>
+          {/* icon */}
+          Info
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MoviePlay;
