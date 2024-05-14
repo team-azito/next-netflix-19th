8c2359ccdfd84dbc0eb1c1733751cab5243926c7
youdame <112458620+youdame@users.noreply.github.com>
2024-05-15 01:25:48 +0900

    Feat : 레이아웃 잡는 중

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,5 +1,19 @@
+import SearchIcon from "#/icons/search/icon-search.svg";
+import CloseIcon from "#/icons/search/icon-close.svg";
 const SearchPage = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="relative">
+        <SearchIcon className="absolute left-16pxr top-15pxr" alt="돋보기 모양 아이콘" />
+        <input
+          className="h-52pxr w-full bg-gray-20 px-50pxr py-20pxr text-gray-30"
+          placeholder="Search for a show, movie, genre, e.t.c."
+        />
+        <CloseIcon className="absolute right-16pxr top-18pxr" alt="검색 결과 되돌리기 아이콘" />
+      </div>
+      <h1>Top Searches</h1>
+    </>
+  );
 };
 
 export default SearchPage;
