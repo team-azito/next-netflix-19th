577977beae114764cd392f209df9db1cf8c759b1
youdame <112458620+youdame@users.noreply.github.com>
2024-05-15 20:11:04 +0900

    Refactor : 클라이언트, 서버 컴포넌트 분리 및 무한스크롤 커스텀 훅 추가 #20

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,45 +1,10 @@
-// "use client";
-import SearchIcon from "#/icons/search/icon-search.svg";
-import CloseIcon from "#/icons/search/icon-close.svg";
-import PlayIcon from "#/icons/search/icon-play.svg";
-import Image from "next/image";
-import SampleImg from "#/images/sample.png";
-import { ChangeEvent, useState } from "react";
-import { getSearchMovie } from "@/api/search";
 import { getMovies } from "@/api/home";
+import SearchMovies from "@/components/search/SearchMovies";
 
 const SearchPage = async () => {
-  const posterBaseUrl = process.env.NEXT_PUBLIC_POSTER_BASE_URL;
+  const { results: popularMoviesData } = await getMovies("popular");
 
-  const popularMovies = await getMovies("popular");
-  return (
-    <div className="w-full">
-      <div className="relative">
-        <SearchIcon className="absolute left-16pxr top-15pxr" alt="돋보기 모양 아이콘" />
-        <input
-          className="h-52pxr w-full bg-gray-20 px-50pxr py-20pxr text-gray-30"
-          placeholder="Search for a show, movie, genre, e.t.c."
-        />
-        {/* <button onClick={() => setSearchInput("")}> */}
-        <CloseIcon className="absolute right-16pxr top-18pxr" alt="검색 결과 되돌리기 아이콘" />
-        {/* </button> */}
-      </div>
-
-      <h1 className=" px-12pxr py-24pxr text-27pxr font-bold">Top Searches</h1>
-
-      {popularMovies.results.map((movie) => (
-        <div key={movie.id} className="flex h-76pxr cursor-pointer bg-gray-20">
-          <div className="relative h-full min-w-146pxr">
-            <Image src={`${posterBaseUrl}${movie.poster_path}`} className="object-cover" fill alt="미디어 이미지" />
-          </div>
-          <div className="flex flex-grow items-center justify-between px-10pxr py-21pxr">
-            <p className="max-w-160pxr text-16pxr">{movie.title}</p>
-            <PlayIcon alt="재생 아이콘" />
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+  return <SearchMovies popularMoviesData={popularMoviesData} />;
 };
 
 export default SearchPage;
---
+++ src/components/search/SearchMovies.tsx
@@ -0,0 +1,42 @@
+import SearchIcon from "#/icons/search/icon-search.svg";
+import CloseIcon from "#/icons/search/icon-close.svg";
+import PlayIcon from "#/icons/search/icon-play.svg";
+import Image from "next/image";
+import { Movie } from "@/types/common";
+
+interface SearchMoviesProps {
+  popularMoviesData: Movie[];
+}
+const SearchMovies = ({ popularMoviesData }: SearchMoviesProps) => {
+  const posterBaseUrl = process.env.NEXT_PUBLIC_POSTER_BASE_URL;
+  return (
+    <div className="w-full">
+      <div className="relative">
+        <SearchIcon className="absolute left-16pxr top-15pxr" alt="돋보기 모양 아이콘" />
+        <input
+          className="h-52pxr w-full bg-gray-20 px-50pxr py-20pxr text-gray-30"
+          placeholder="Search for a show, movie, genre, e.t.c."
+        />
+        <button>
+          <CloseIcon className="absolute right-16pxr top-18pxr" alt="검색 결과 되돌리기 아이콘" />
+        </button>
+      </div>
+
+      <h1 className=" px-12pxr py-24pxr text-27pxr font-bold">Top Searches</h1>
+
+      {popularMoviesData.map((movie) => (
+        <div key={movie.id} className="flex h-76pxr cursor-pointer bg-gray-20">
+          <div className="relative h-full min-w-146pxr">
+            <Image src={`${posterBaseUrl}${movie.poster_path}`} className="object-cover" fill alt="미디어 이미지" />
+          </div>
+          <div className="flex flex-grow items-center justify-between px-10pxr py-21pxr">
+            <p className="max-w-160pxr text-16pxr">{movie.title}</p>
+            <PlayIcon alt="재생 아이콘" />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default SearchMovies;
